0e84bd92207b87aa0b8b2f6bf6ee3e598b74bea0
Daoma666 <592455811@qq.com>
2018-06-29 22:25:26 +0800

    h

--- src/index.js
+++ src/index.js
@@ -6,7 +6,10 @@ import {TabSelectorSample} from './c2/TabSelector';
 import Clock from './c4/Clock';
 import SnapshotSample from './c4/SnapshotSample';
 import {AdvancedTabSelectorSample} from './c6/AdvancedTabSelector';
+import LocaleSample from './c7/LocaleSample';
 import './index.css';
 
-ReactDOM.render(<AdvancedTabSelectorSample />, document.getElementById('root'));
+
+
+ReactDOM.render(<LocaleSample />, document.getElementById('root'));
 
